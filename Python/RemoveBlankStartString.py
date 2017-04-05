f7c8b5f0a340ba1c83b3999635c6c3b3664733ec
HolyFredy <flavizen@gmail.com>
2017-04-05 13:23:34 +0200

    buildIn function in ptyhon replacement
    a function is write in python code source

--- Python/RemoveBlankStartString.py
+++ Python/RemoveBlankStartString.py
@@ -1,10 +1,15 @@
 # Remove left space(s) in string
 
-def avd_1st_space(string):
+'''def avd_1st_space(string):
     return string.split(None, 0)[0]
 
 str = "       Hello, i\'m a horse !"
 
-print(avd_1st_space(str))
+print(avd_1st_space(str))'''
+
+# Code above has been replace by this instead
+
+str = "       Hello, i\'m a horse !"
+str.lstrip()
 
 # [OUTPUT] --> "Hello, i'm a horse !"
